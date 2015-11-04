94555d4aacfcb3d648f3668dd5eaf05ea60af657
José Luiz Martins Oliveria <martinsmanager@gmail.com>
2015-11-04 11:23:33 -0200

    Ajustar header enviado pelo CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,26 +15,25 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        
-        header("Access-Control-Allow-Origin: *");
-        //res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
 
         // ALLOW OPTIONS METHOD
         $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, Authorization'
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Origin' => '*',
+
         ];
-        if($request->getMethod() == "OPTIONS") {
+        if ($request->getMethod() == 'OPTIONS') {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return response('OK', 200, $headers);
         }
 
         $response = $next($request);
-        
-        foreach($headers as $key => $value) 
-            $response->header($key, $value);
-        
-        return $response;
 
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
     }
 }
