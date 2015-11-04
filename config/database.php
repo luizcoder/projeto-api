c7ccda84c9d71ac523a7f235cccd583091ddc2fb
José Luiz Martins Oliveria <martinsmanager@gmail.com>
2015-11-04 11:30:13 -0200

    Adicionar conexão com sqlite-memory

--- config/database.php
+++ config/database.php
@@ -52,6 +52,12 @@ return [
             'prefix'   => '',
         ],
 
+        'sqlite-memory' => [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ],
+
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
